1a4db23d07cc48ee5a52731c40a1f10611c790be
Arai <t.arai.sys@morijyobi.ac.jp>
2022-09-21 12:21:38 +0900

    プログラムを変更しました。

--- pra2.java
+++ pra2.java
@@ -1,5 +1,7 @@
 public class kadai101 {
 	public static void main(String[] args) {
-		System.out.print("hello world");
+		for(int i = 0 ; i<100 ; i++){
+			System.out.println("Hello world");
+		}
 	}
 }
